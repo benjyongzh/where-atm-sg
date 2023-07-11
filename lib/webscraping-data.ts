0daf25e37bb01831738aff20385097c198396396
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-11 16:07:21 +0800

    Create endpoint url info.

---
+++ lib/webscraping-data.ts
@@ -0,0 +1,74 @@
+export const baseBankInfosUrl = "https://sgbanks.com/banks/";
+export const baseBankAtmListUrl = "https://sgbanks.com/branches/";
+
+export const divClassName = "lstbranches";
+
+export interface bankEndpoint {
+  bankInfo: string;
+  atmList: string;
+}
+
+const dbsEndpoints: bankEndpoint = {
+  bankInfo: "dbs-bank",
+  atmList: "dbs-bank-atms-singapore",
+};
+
+const uobEndpoints: bankEndpoint = {
+  bankInfo: "uob-singapore",
+  atmList: "uob-singapore-atms",
+};
+
+const citibankEndpoints: bankEndpoint = {
+  bankInfo: "citibank-singapore",
+  atmList: "citibank-singapore-atms",
+};
+
+const maybankEndpoints: bankEndpoint = {
+  bankInfo: "maybank-singapore",
+  atmList: "maybank-singapore-branches-atms",
+};
+
+const standardCharteredEndpoints: bankEndpoint = {
+  bankInfo: "standard-chartered-singapore",
+  atmList: "standard-chartered-singapore-atms",
+};
+
+const ocbcEndpoints: bankEndpoint = {
+  bankInfo: "ocbc-singapore-bank",
+  atmList: "ocbc-singapore-bank-branches-atms",
+};
+
+const hsbcEndpoints: bankEndpoint = {
+  bankInfo: "hsbc-singapore",
+  atmList: "hsbc-singapore-branches-atms",
+};
+
+const anzEndpoints: bankEndpoint = {
+  bankInfo: "anz-singapore-bank",
+  atmList: "anz-singapore-branches-atms",
+};
+
+const cimbEndpoints: bankEndpoint = {
+  bankInfo: "cimb-bank-singapore",
+  atmList: "cimb-bank-singapore-branches",
+};
+
+const rhbEndpoints: bankEndpoint = {
+  bankInfo: "rhb-singapore-bank",
+  atmList: "rhb-singapore-branches",
+};
+
+const bankEndpoints = {
+  dbs: dbsEndpoints,
+  uob: uobEndpoints,
+  citibank: citibankEndpoints,
+  maybank: maybankEndpoints,
+  standardChartered: standardCharteredEndpoints,
+  ocbc: ocbcEndpoints,
+  hsbc: hsbcEndpoints,
+  anz: anzEndpoints,
+  cimb: cimbEndpoints,
+  rhb: rhbEndpoints,
+};
+
+export default bankEndpoints;
