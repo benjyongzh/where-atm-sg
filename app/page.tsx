b784d635d4501c057b587b98ad59854c22da5531
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-12 22:22:38 +0800

    add errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
+import { errorMessageObject } from "@/lib/errors";
 import { getAllAtmData } from "@/lib/load-atm-data";
 import { rawAtmInfo } from "@/lib/webscraping-data";
 
 export default async function Home() {
   const data = await getAllAtmData();
-  const { atmList } = data;
+  const { atmList, errors } = data;
+
+  //save atmList to redux?
 
   return data ? (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      {atmList.map((atm: rawAtmInfo) => (
-        <div>
-          <div>{atm.name}</div>
+      {errors.map((error: errorMessageObject, index: number) => (
+        <div key={index}>
+          <div>{error.errorMessage}</div>
+        </div>
+      ))}
+      {atmList.map((atm: rawAtmInfo, index: number) => (
+        <div key={index}>
+          <div>{atm.location}</div>
           <div>{atm.brand}</div>
           <div>{atm.address}</div>
           <div>{atm.postalCode}</div>
