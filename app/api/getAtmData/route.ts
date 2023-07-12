03d33103a0764f199e29351580a3be385c4fc68e
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-12 17:10:04 +0800

    Fix data being empty.

--- app/api/getAtmData/route.ts
+++ app/api/getAtmData/route.ts
@@ -1,43 +1,73 @@
 import { NextRequest, NextResponse } from "next/server";
-import bankEndpoints, {
-  rawAtmInfo,
-  baseBankAtmListUrl,
-  divClassName,
-} from "@/lib/webscraping-data";
-import { JSDOM } from "jsdom";
+import { rawAtmInfo, isRawAtmInfo } from "@/lib/webscraping-data";
+import { getBankAtmList } from "@/lib/load-atm-data";
+
+import { isArrayOfInterface } from "@/utils/objects";
+
+import { errorMessageObject, isErrorMessageObject } from "@/lib/errors";
 
 export async function GET(req: NextRequest) {
   try {
-    //webscraping is done here
-    // const result: AtmData = { message: "john doe" };
-    const dbsAtms = await fetch(
-      baseBankAtmListUrl + bankEndpoints.dbs.atmList
-    ).then((data) => data.text());
+    let atmList: rawAtmInfo[] = [];
 
-    const dom = new JSDOM(dbsAtms);
-    const document = dom.window.document;
-    const atmDocList = document.querySelector(`.${divClassName}`)?.children;
-    if (atmDocList === undefined)
-      return new Response(
-        JSON.stringify({ message: "Missing data extracted" })
-      );
+    let dbsAtms: rawAtmInfo[] | errorMessageObject = await getBankAtmList(
+      "DBS"
+    );
+    if (Array.isArray(dbsAtms)) atmList = atmList.concat(dbsAtms);
 
-    let atmDocArray: rawAtmInfo[] = [];
-    for (let i = 0; i < atmDocList!.length; i++) {
-      const text = atmDocList![i].textContent;
-      if (text !== "") {
-        const textSplit = text!.split("\n");
-        const atmInfo: rawAtmInfo = {
-          name: textSplit[0],
-          address: textSplit[1],
-          postalCode: textSplit[2],
-        };
-        atmDocArray.push(atmInfo); //second console output
-      }
-    }
-    console.log(atmDocArray);
+    let uobAtms: rawAtmInfo[] | errorMessageObject = await getBankAtmList(
+      "UOB"
+    );
+    if (Array.isArray(uobAtms)) atmList = atmList.concat(uobAtms);
 
-    return new Response(JSON.stringify({ atmList: atmDocArray }));
+    let citibankAtms: rawAtmInfo[] | errorMessageObject = await getBankAtmList(
+      "CitiBank"
+    );
+
+    if (Array.isArray(citibankAtms)) atmList = atmList.concat(citibankAtms);
+
+    let maybankAtms: rawAtmInfo[] | errorMessageObject = await getBankAtmList(
+      "MayBank"
+    );
+    if (Array.isArray(maybankAtms)) atmList = atmList.concat(maybankAtms);
+
+    let standardcharteredAtms: rawAtmInfo[] | errorMessageObject =
+      await getBankAtmList("StandardChartered");
+    if (Array.isArray(standardcharteredAtms))
+      atmList = atmList.concat(standardcharteredAtms);
+
+    let ocbcAtms: rawAtmInfo[] | errorMessageObject = await getBankAtmList(
+      "OCBC"
+    );
+    if (Array.isArray(ocbcAtms)) atmList = atmList.concat(ocbcAtms);
+
+    let hsbcAtms: rawAtmInfo[] | errorMessageObject = await getBankAtmList(
+      "HSBC"
+    );
+    if (Array.isArray(hsbcAtms)) atmList = atmList.concat(hsbcAtms);
+
+    let anzAtms: rawAtmInfo[] | errorMessageObject = await getBankAtmList(
+      "ANZ"
+    );
+    if (Array.isArray(anzAtms)) atmList = atmList.concat(anzAtms);
+
+    let cimbAtms: rawAtmInfo[] | errorMessageObject = await getBankAtmList(
+      "CIMB"
+    );
+    if (Array.isArray(cimbAtms)) atmList = atmList.concat(cimbAtms);
+
+    let rhbAtms: rawAtmInfo[] | errorMessageObject = await getBankAtmList(
+      "RHB"
+    );
+    if (Array.isArray(rhbAtms)) atmList = atmList.concat(rhbAtms);
+
+    console.log("total number of ATMs: ", atmList.length);
+
+    return new Response(
+      JSON.stringify({
+        atmList,
+      })
+    );
   } catch (err) {
     return new Response(JSON.stringify({ message: "failed to load ATM data" }));
   }
--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,19 @@ import { rawAtmInfo } from "@/lib/webscraping-data";
 
 export default async function Home() {
   const data = await getAllAtmData();
+  const { atmList } = data;
 
   return data ? (
-    data.atmList ? (
-      <main className="flex flex-col items-center justify-between min-h-screen p-24">
-        {data.atmList.map((atm: rawAtmInfo) => (
-          <div>
-            <div>{atm.name}</div>
-            <div>{atm.address}</div>
-            <div>{atm.postalCode}</div>
-          </div>
-        ))}
-      </main>
-    ) : (
-      <main className="flex flex-col items-center justify-between min-h-screen p-24">
-        {data.message}
-      </main>
-    )
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      {atmList.map((atm: rawAtmInfo) => (
+        <div>
+          <div>{atm.name}</div>
+          <div>{atm.brand}</div>
+          <div>{atm.address}</div>
+          <div>{atm.postalCode}</div>
+        </div>
+      ))}
+    </main>
   ) : (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       loading
