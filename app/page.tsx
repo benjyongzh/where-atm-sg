a786817e349ebb98113b05f746eeac6aeaad2c98
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-13 16:24:56 +0800

    Add range setting component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { errorMessageObject } from "@/lib/errors";
 import { getAllAtmData, bankNameList } from "@/lib/load-atm-data";
 
+//components
 import FilterButton from "@/components/FilterButton";
 import AtmList from "@/components/AtmList";
+import RangeSetting from "@/components/RangeSetting";
 
 export default async function Home() {
   //save atmList to redux?
@@ -12,6 +14,10 @@ export default async function Home() {
   return (
     <main className="flex flex-col items-stretch justify-start min-h-screen gap-8 p-24">
       <header className="w-full m-5 text-xl text-center">Where ATM SG?</header>
+      <div className="flex items-center justify-center gap-3 p-5">
+        <RangeSetting />
+      </div>
+
       <div className="flex flex-wrap items-center justify-center gap-3 p-5">
         {bankNameList.map((bank) => (
           <FilterButton key={bank + "filterbutton"} name={bank} />
