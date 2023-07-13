36c1d598bf85ccb74d158c7ddca4b9140caa4e87
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-13 11:41:22 +0800

    Use FilterButton and AtmList

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,37 @@
 import { errorMessageObject } from "@/lib/errors";
-import { getAllAtmData } from "@/lib/load-atm-data";
+import { getAllAtmData, bankNameList } from "@/lib/load-atm-data";
 import { rawAtmInfo } from "@/lib/webscraping-data";
 
+import FilterButton from "@/components/FilterButton";
+import AtmList from "@/components/AtmList";
+
+// import { useAppSelector, useAppDispatch } from "@/hooks/reduxHooks";
+// import { setAtmData } from "@/features/atmData/atmDataSlice";
+
 export default async function Home() {
-  const data = await getAllAtmData();
-  const { atmList, errors } = data;
-
   //save atmList to redux?
+  // const dispatch = useAppDispatch();
+  // const storedAtmData = useAppSelector((state) => state.atmData.allAtms);
+  const dataToUse = await getAllAtmData();
+  // dispatch(setAtmData(dataToUse));
+  const { atmList, errors } = dataToUse;
 
-  return data ? (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+  return atmList.length || errors.length ? (
+    <main className="flex flex-col items-stretch justify-start min-h-screen gap-8 p-24">
+      <header className="w-full m-5 text-xl text-center">Where ATM SG?</header>
+      <div className="flex flex-wrap items-center justify-center gap-3 p-5">
+        {bankNameList.map((bank) => (
+          <FilterButton key={bank + "filterbutton"} name={bank} />
+        ))}
+      </div>
+
       {errors.map((error: errorMessageObject, index: number) => (
         <div key={index}>
           <div>{error.errorMessage}</div>
         </div>
       ))}
-      {atmList.map((atm: rawAtmInfo, index: number) => (
-        <div key={index}>
-          <div>{atm.location}</div>
-          <div>{atm.brand}</div>
-          <div>{atm.address}</div>
-          <div>
-            {atm.info?.map((infoline, i) => (
-              <div key={i}>{infoline}</div>
-            ))}
-          </div>
-        </div>
-      ))}
+
+      <AtmList />
     </main>
   ) : (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
