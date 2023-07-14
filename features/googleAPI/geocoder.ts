a3157106c08cc6a5d737c2163e311b7fa112b5e5
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-14 22:04:43 +0800

    Create separate functions for geocoding only

---
+++ features/googleAPI/geocoder.ts
@@ -0,0 +1,33 @@
+// var geocoder;
+
+import { IGeoCode } from "../settings/settingsSlice";
+
+// geocoder = new google.maps.Geocoder();
+
+export async function getAddressGeocoded(address: string) {
+  const res = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GMAPS_API_KEY}`
+  );
+  const data = await res.json();
+  return data;
+
+  /* geocoder.geocode({ address: address }, function (results, status) {
+    if (status == "OK") {
+      console.log(results[0].geometry.location);
+      // map.setCenter(results[0].geometry.location);
+      //   var marker = new google.maps.Marker({
+      //       map: map,
+      //       position: results[0].geometry.location
+      //   });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  }); */
+}
+
+export function getLatLongFromGeoCodeResult(res: any): IGeoCode {
+  return {
+    lat: res.geometry.location.lat,
+    long: res.geometry.location.lng,
+  };
+}
