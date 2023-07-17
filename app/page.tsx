5337dd17515d60206c250e4f02d914dc8206625d
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-17 15:19:42 +0800

    Styling improvement for buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,26 @@
 //components
 import Link from "next/link";
+import { BiCurrentLocation, BiSearch } from "react-icons/bi";
 
 export default async function Home() {
   return (
-    <main className="flex flex-col gap-6 sm:gap-8 main-bg">
-      <header className="text-4xl text-center page-header">
+    <main className="flex flex-col items-center justify-start gap-10 overflow-hidden pt-52 main-bg">
+      <header className="mt-0 text-4xl text-center page-header">
         Where ATM SG?
       </header>
+      {/* subheader*/}
+      <div className="flex items-center justify-center -mt-5 text-center section">
+        Find your nearest ATM with ease and simplicity.
+      </div>
       {/* buttons for nav */}
-      <div className="flex flex-col items-center justify-center gap-5 sm:flex-row">
-        <Link href="/currentLoc" className="w-full btn btn-primary sm:w-36">
-          Use my location
+      <div className="flex flex-col items-center justify-center w-full gap-3 sm:max-w-md sm:gap-5 sm:flex-row section">
+        <Link href="/useLocation" className="w-full gap-3 btn btn-primary">
+          <BiCurrentLocation />
+          <span>Use my location</span>
         </Link>
-        <Link href="/staticLoc" className="w-full btn btn-secondary sm:w-36">
-          Search Address
+        <Link href="/searchAddress" className="w-full gap-3 btn btn-secondary">
+          <BiSearch />
+          <span>Search Address</span>
         </Link>
       </div>
     </main>
