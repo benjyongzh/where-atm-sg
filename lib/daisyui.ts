16fa48c63ed8160d5784c81707f76aef675fefdd
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-18 16:22:00 +0800

    Add light and night themes for reference

--- lib/daisyui.ts
+++ lib/daisyui.ts
@@ -56,3 +56,51 @@ export function extractThemeColorsFromDOM(): ThemeColors {
     errorContent: `hsl(${computedStyles.getPropertyValue("--erc")}`,
   };
 }
+
+/* cupcake theme
+"[data-theme=cupcake]": {
+  "color-scheme": "light",
+  "primary": "#65c3c8",
+  "secondary": "#ef9fbc",
+  "accent": "#eeaf3a",
+  "neutral": "#291334",
+  "base-100": "#faf7f5",
+  "base-200": "#efeae6",
+  "base-300": "#e7e2df",
+  "base-content": "#291334",
+  "--rounded-btn": "1.9rem",
+  "--tab-border": "2px",
+  "--tab-radius": ".5rem",
+}, */
+
+/* "[data-theme=light]": {
+  "color-scheme": "light",
+  "primary": "#570df8",
+  "primary-content": "#E0D2FE",
+  "secondary": "#f000b8",
+  "secondary-content": "#FFD1F4",
+  "accent": "#1ECEBC",
+  "accent-content": "#07312D",
+  "neutral": "#2B3440",
+  "neutral-content": "#D7DDE4",
+  "base-100": "#ffffff",
+  "base-200": "#F2F2F2",
+  "base-300": "#E5E6E6",
+  "base-content": "#1f2937",
+}, */
+
+/* 
+"[data-theme=night]": {
+  "color-scheme": "dark",
+  "primary": "#38bdf8",
+  "secondary": "#818CF8",
+  "accent": "#F471B5",
+  "neutral": "#1E293B",
+  "neutral-focus": "#273449",
+  "base-100": "#0F172A",
+  "info": "#0CA5E9",
+  "info-content": "#000000",
+  "success": "#2DD4BF",
+  "warning": "#F4BF50",
+  "error": "#FB7085",
+}, */
