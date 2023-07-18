1e907e9eeaec5ca255f986ff10614de9cfbe841a
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-18 16:22:35 +0800

    Add distance and info separately.

--- components/MapInfoWindowData.tsx
+++ components/MapInfoWindowData.tsx
@@ -1,18 +1,22 @@
-import React from "react";
+import { GiPathDistance } from "react-icons/gi";
 
 type InfoWindowProps = {
   title: string;
   address: string;
-  info: string;
+  distance: number;
+  info?: string[];
 };
 
 const MapInfoWindowData = (props: InfoWindowProps) => {
-  const { title, address, info } = props;
+  const { title, address, distance, info } = props;
   return (
-    <div>
-      <p>{title}</p>
+    <div className="flex flex-col gap-2">
+      <p className="text-lg font-bold">{title}</p>
       <p>{address}</p>
-      <p>{info}m</p>
+      <div className="flex items-center justify-start gap-3">
+        <GiPathDistance />
+        {distance}m
+      </div>
     </div>
   );
 };
