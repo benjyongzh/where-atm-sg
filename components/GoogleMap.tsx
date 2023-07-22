357928388901fd965addeb23feeaf214e451b4c6
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-22 16:23:31 +0800

    Create mapmarker component

--- components/GoogleMap.tsx
+++ components/GoogleMap.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useRef, useState, useCallback, useEffect } from "react";
 import {
   GoogleMap,
   useJsApiLoader,
@@ -33,118 +33,13 @@ import {
   getBrandFromRawPlacesInfo,
 } from "@/lib/atmObject";
 import { haversine_distance } from "@/utils/distance";
-
-type GoogleMapsProps = {
-  center: IGeoCode;
-  atms: IAtmObject[];
-  selectAtm: Function;
-  selectedAtmId: string | null;
-};
-
-// export default function GoogleMaps(props: GoogleMapsProps) {
-//   const googleMapsApiKey = process.env.NEXT_PUBLIC_GMAPS_API_KEY;
-
-//   if (googleMapsApiKey === undefined) {
-//     return <div>Error. Invalid API key</div>;
-//   }
-//   return <Map googleMapsApiKey={googleMapsApiKey} atms={props.atms} />;
-// }
-
-// type MapProps = {
-//   googleMapsApiKey: string;
-//   atms: IAtmObject[];
-// };
-
-// function Map(props: MapProps) {
-//   // infers type from default value
-//   const center: IGeoCode = useAppSelector(
-//     (state) => state.settings.searchLocationPoint
-//   );
-//   const storedRange = useAppSelector((state) => state.settings.maxRange);
-//   const [selectedMarker, setSelectedMarker] = useState<IGeoCode | null>(center);
-//   const { googleMapsApiKey, atms } = props;
-
-//   const { isLoaded } = useLoadScript({
-//     googleMapsApiKey: googleMapsApiKey,
-//   });
-//   if (!isLoaded) return <div>Loading...</div>;
-
-//   return (
-//     <div className="mx-20 my-8 md:my-10">
-//       <GoogleMap
-//         zoom={10} //zoom integer setting
-//         center={center}
-//         mapContainerClassName="googlemap-container"
-//       >
-//         {/* center marking */}
-//         {/* <MarkerF
-//           position={center} //marker position
-//           onClick={() => {
-//             setSelectedMarker(center); //marker position
-//           }}
-//         >
-//           {selectedMarker && (
-//             <InfoWindow
-//               onCloseClick={() => {
-//                 setSelectedMarker(null);
-//               }}
-//               position={center} //marker position
-//             >
-//               <p>Directions</p>
-//               <p>This is your input address</p>
-//             </InfoWindow>
-//           )}
-//         </MarkerF> */}
-
-//         {/* <CircleF
-//           options={{
-//             strokeColor: "hsl(var(--p))",
-//             strokeOpacity: 0.7,
-//             strokeWeight: 2,
-//             fillColor: "hsl(var(--p))",
-//             fillOpacity: 0.2,
-//             clickable: false,
-//             draggable: false,
-//             editable: false,
-//             visible: true,
-//             zIndex: 1,
-//           }}
-//           center={center}
-//           radius={storedRange}
-//         ></CircleF> */}
-
-//         {/* set atm locations across map */}
-//         {atms.map((atm) => (
-//           <MarkerF
-//             position={atm.location} //marker position
-//             onClick={() => {
-//               setSelectedMarker(atm.location); //marker position
-//             }}
-//           >
-//             {selectedMarker && (
-//               <InfoWindow
-//                 onCloseClick={() => {
-//                   setSelectedMarker(null);
-//                 }}
-//                 position={atm.location} //marker position
-//               >
-//                 <p>{atm.brand}</p>
-//                 <p>{atm.name}</p>
-//                 <p>{atm.address}</p>
-//                 <p>{atm.distance}m away</p>
-//               </InfoWindow>
-//             )}
-//           </MarkerF>
-//         ))}
-//       </GoogleMap>
-//     </div>
-//   );
-// }
+import MapMarker from "./MapMarker";
 
 function GoogleMaps() {
   //redux
   const dispatch = useAppDispatch();
   const storedRange = useAppSelector((state) => state.settings.maxRange);
+  const latestSearchRange = useRef(storedRange);
   const storedSelectedAtmId = useAppSelector(
     (state) => state.atmData.selectedAtmPlaceId
   );
@@ -177,12 +72,12 @@ function GoogleMaps() {
   // const { atms, center, selectAtm, selectedAtmId } = props;
   const [map, setMap] = useState<any>(null);
 
-  const zoomIndex = currentBreakpoint === "xs" ? 10 : 11;
+  const initialZoom = currentBreakpoint === "xs" ? 10 : 11;
 
   const onLoad = useCallback(function callback(map: any) {
     // const bounds = new window.google.maps.LatLngBounds(mapCenterDefault);
     // map.fitBounds(bounds);
-    map.setZoom(zoomIndex);
+    map.setZoom(initialZoom);
     setMap(map);
   }, []);
 
@@ -214,6 +109,26 @@ function GoogleMaps() {
       }
     : undefined;
 
+  const allAtmList = fullAtmList
+    .map((atm: rawFetchedNearbyPlacesInfo): IAtmObject => {
+      const atmBrand = getBrandFromRawPlacesInfo(atm);
+
+      // log distances from each ATM
+      const distance = haversine_distance(storedSearchPoint, atm.location);
+      return {
+        brand: atmBrand,
+        name: atm.name,
+        location: atm.location,
+        place_id: atm.place_id,
+        address: atm.vicinity,
+        distance,
+      };
+    })
+    .filter((atm: IAtmObject): boolean => {
+      return !storedBankFilter.includes(atm.brand) && atm.brand !== "";
+    })
+    .sort((atmA, atmB) => atmA.distance! - atmB.distance!); //sort from shortest distance to longest
+
   const filteredAtmList = fullAtmList
     .map((atm: rawFetchedNearbyPlacesInfo): IAtmObject => {
       const atmBrand = getBrandFromRawPlacesInfo(atm);
@@ -232,8 +147,8 @@ function GoogleMaps() {
     .filter((atm: IAtmObject): boolean => {
       return !storedBankFilter.includes(atm.brand) && atm.brand !== "";
     })
-    .filter((atm) => atm.distance <= storedRange) //only use ATMs in range
-    .sort((atmA, atmB) => atmA.distance! - atmB.distance!); //sort from shortest distance to longest
+    .sort((atmA, atmB) => atmA.distance! - atmB.distance!) //sort from shortest distance to longest
+    .filter((atm) => atm.distance <= storedRange); //only use ATMs in range
 
   const handleSelectAtmMarker = (atm: IAtmObject | null) => {
     if (atm === null) {
@@ -256,14 +171,60 @@ function GoogleMaps() {
 
   useEffect(() => {
     mapLookAt(storedSearchPoint);
+    mapFitFilteredAtms();
+    //create new static circle
+    latestSearchRange.current = storedRange;
   }, [storedSearchPoint]);
 
+  const staticCircle =
+    isLoaded && searchStarted ? (
+      <CircleF
+        options={{
+          strokeColor: cupcakeColours.neutral,
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: cupcakeColours.neutral,
+          fillOpacity: 0,
+          clickable: false,
+          draggable: false,
+          editable: false,
+          visible: true,
+          zIndex: 1,
+        }}
+        center={storedSearchPoint}
+        radius={latestSearchRange.current}
+      ></CircleF>
+    ) : null;
+
   const mapLookAt = (point: IGeoCode) => {
-    if (map) {
-      map.panTo(point);
-      // map.setZoom(zoomIndex);
-    }
+    console.log(point);
+    console.log(map);
+    if (map) map.panTo(point);
   };
+
+  const mapFitFilteredAtms = () => {
+    if (!isLoaded) return;
+    const bounds = new google.maps.LatLngBounds();
+    filteredAtmList.forEach((atm) => bounds.extend(atm.location));
+    map.fitBounds(bounds);
+    // map.fitBounds(searchCircle.getBounds());
+  };
+
+  /*   const searchCircle = new google.maps.Circle({
+    strokeColor: cupcakeColours.primary,
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: cupcakeColours.primary,
+    fillOpacity: 0.1,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    visible: true,
+    zIndex: 1,
+    map,
+    center: storedSearchPoint,
+    radius: storedRange,
+  }); */
 
   return isLoaded ? (
     <GoogleMap
@@ -272,7 +233,7 @@ function GoogleMaps() {
         height: "100%",
       }}
       center={mapCenterDefault}
-      zoom={zoomIndex}
+      zoom={initialZoom}
       onLoad={onLoad}
       onUnmount={onUnmount}
       options={{
@@ -303,31 +264,38 @@ function GoogleMaps() {
         ></CircleF>
       ) : null}
 
+      {/* center marking static */}
+      {staticCircle}
+
       {/* ATMs found */}
-      {filteredAtmList.map((atm) => (
-        <MarkerF
-          position={atm.location} //marker position
-          onClick={() => handleSelectAtmMarker(atm)}
-          icon={
-            atm.distance <= storedRange ? svgMarkerInRange : svgMarkerOutOfRange
-          }
-          key={atm.place_id}
-        >
-          {storedSelectedAtmId === atm.place_id && (
-            <InfoWindow
-              onCloseClick={() => handleSelectAtmMarker(null)}
-              position={atm.location} //marker position
-            >
-              <MapInfoWindowData
-                title={atm.brand}
-                address={atm.address}
-                distance={atm.distance}
-                info={atm.info}
-              />
-            </InfoWindow>
-          )}
-        </MarkerF>
-      ))}
+      {allAtmList.map(
+        (atm) =>
+          isLoaded && (
+            <MapMarker atm={atm} handleSelect={handleSelectAtmMarker} />
+          )
+        // {/* <MarkerF
+        //   position={atm.location} //marker position
+        //   onClick={() => handleSelectAtmMarker(atm)}
+        //   icon={
+        //     atm.distance <= storedRange ? svgMarkerInRange : svgMarkerOutOfRange
+        //   }
+        //   key={atm.place_id}
+        // >
+        //   {storedSelectedAtmId === atm.place_id && (
+        //     <InfoWindow
+        //       onCloseClick={() => handleSelectAtmMarker(null)}
+        //       position={atm.location} //marker position
+        //     >
+        //       <MapInfoWindowData
+        //         title={atm.brand}
+        //         address={atm.address}
+        //         distance={atm.distance}
+        //         info={atm.info}
+        //       />
+        //     </InfoWindow>
+        //   )}
+        // </MarkerF> */}
+      )}
     </GoogleMap>
   ) : (
     <div className="flex items-center justify-center w-full h-full gap-3">
---
+++ components/MapMarker.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+//redux
+import { useAppSelector } from "@/hooks/reduxHooks";
+
+import { MarkerF, InfoWindow } from "@react-google-maps/api";
+import { IAtmObject } from "@/lib/atmObject";
+import MapInfoWindowData from "./MapInfoWindowData";
+import daisyuiColors from "daisyui/src/theming/themes";
+const cupcakeColours = daisyuiColors["[data-theme=cupcake]"];
+const lightColours = daisyuiColors["[data-theme=light]"];
+const nightColours = daisyuiColors["[data-theme=night]"];
+
+type MarkerProps = {
+  atm: IAtmObject;
+  handleSelect: Function;
+};
+
+const MapMarker = (props: MarkerProps) => {
+  const { atm, handleSelect } = props;
+  const [inRange, setInRange] = useState(false);
+  const storedRange = useAppSelector((state) => state.settings.maxRange);
+  const storedSelectedAtmId = useAppSelector(
+    (state) => state.atmData.selectedAtmPlaceId
+  );
+
+  const svgMarkerInRange = {
+    path: "M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z",
+    fillColor: cupcakeColours.primary,
+    fillOpacity: 0.9,
+    strokeWeight: 2,
+    strokeColor: nightColours.info,
+    scale: 2,
+    anchor: new google.maps.Point(12, 21),
+  };
+
+  const svgMarkerOutOfRange = {
+    path: "M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z",
+    fillColor: cupcakeColours.secondary,
+    fillOpacity: 0.9,
+    strokeWeight: 2,
+    strokeColor: nightColours.error,
+    scale: 2,
+    anchor: new google.maps.Point(12, 21),
+  };
+
+  useEffect(() => {
+    setInRange(atm.distance <= storedRange);
+    console.log(`${atm.brand} is in range? `, inRange);
+  }, [storedRange]);
+
+  return (
+    <MarkerF
+      position={atm.location} //marker position
+      onClick={() => handleSelect(atm)}
+      icon={inRange ? svgMarkerInRange : svgMarkerOutOfRange}
+      key={atm.place_id}
+    >
+      {storedSelectedAtmId === atm.place_id && (
+        <InfoWindow
+          onCloseClick={() => handleSelect(null)}
+          position={atm.location} //marker position
+        >
+          <MapInfoWindowData
+            title={atm.brand}
+            address={atm.address}
+            distance={atm.distance}
+            info={atm.info}
+          />
+        </InfoWindow>
+      )}
+    </MarkerF>
+  );
+};
+
+export default MapMarker;
