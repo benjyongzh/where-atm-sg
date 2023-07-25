2b0b1fc6382bfe8d191f029497dcec034b6acf35
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-25 20:40:56 +0800

    Add try catch

--- features/googleAPI/geocoder.ts
+++ features/googleAPI/geocoder.ts
@@ -6,24 +6,15 @@ export interface IGeoCode {
 // geocoder = new google.maps.Geocoder();
 
 export async function getAddressGeocoded(address: string) {
-  const res = await fetch(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GMAPS_API_KEY}`
-  );
-  const data = await res.json();
-  return data;
-
-  /* geocoder.geocode({ address: address }, function (results, status) {
-    if (status == "OK") {
-      console.log(results[0].geometry.location);
-      // map.setCenter(results[0].geometry.location);
-      //   var marker = new google.maps.Marker({
-      //       map: map,
-      //       position: results[0].geometry.location
-      //   });
-    } else {
-      alert("Geocode was not successful for the following reason: " + status);
-    }
-  }); */
+  try {
+    const res = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GMAPS_API_KEY}`
+    );
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(`Geocoding error: `, err);
+  }
 }
 
 export function getLatLongFromGeoCodeResult(res: any): IGeoCode {
