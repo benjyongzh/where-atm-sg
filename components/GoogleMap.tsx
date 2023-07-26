f9702b098cf3e7aa7ba9de63eb5ca58e01bbfe75
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-26 15:33:58 +0800

    Remove unused modules

--- components/GoogleMap.tsx
+++ components/GoogleMap.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { /* useRef,  */ useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import {
   GoogleMap,
   useJsApiLoader,
@@ -12,14 +12,10 @@ import {
 //redux
 import { useAppSelector, useAppDispatch } from "@/hooks/reduxHooks";
 import { setSelectedAtmPlaceId } from "@/features/atmData/atmDataSlice";
-import {
-  mapCenterDefault,
-  //setMapCentrePoint,
-} from "@/features/settings/settingsSlice";
+import { mapCenterDefault } from "@/features/settings/settingsSlice";
 import { getGMapsAPIKey } from "@/features/googleAPI/key";
 
 //daisyUI
-import MapInfoWindowData from "./MapInfoWindowData";
 import daisyuiColors from "daisyui/src/theming/themes";
 const cupcakeColours = daisyuiColors["[data-theme=cupcake]"];
 const lightColours = daisyuiColors["[data-theme=light]"];
@@ -27,19 +23,13 @@ const nightColours = daisyuiColors["[data-theme=night]"];
 
 //lib/utils
 import { IGeoCode } from "@/features/googleAPI/geocoder";
-import {
-  IAtmObject,
-  rawFetchedNearbyPlacesInfo,
-  getBrandFromRawPlacesInfo,
-} from "@/lib/atmObject";
-import { haversine_distance } from "@/utils/distance";
+import { IAtmObject } from "@/lib/atmObject";
 import MapMarker from "./MapMarker";
 
 function GoogleMaps() {
   //redux
   const dispatch = useAppDispatch();
   const storedRange = useAppSelector((state) => state.settings.maxRange);
-  // const latestSearchRange = useRef(storedRange);
   const storedSelectedAtmId = useAppSelector(
     (state) => state.atmData.selectedAtmPlaceId
   );
@@ -53,10 +43,6 @@ function GoogleMaps() {
     (state) => state.settings.searchLocationPoint
   );
 
-  /* const storedMapCentrePoint: IGeoCode = useAppSelector(
-    (state) => state.settings.mapCentrePoint
-  ); */
-
   const searchStarted: boolean = useAppSelector(
     (state) => state.atmData.searchStarted
   );
@@ -69,7 +55,6 @@ function GoogleMaps() {
     id: "google-map-script",
     googleMapsApiKey: process.env.NEXT_PUBLIC_GMAPS_API_KEY!, //API key
   });
-  // const { atms, center, selectAtm, selectedAtmId } = props;
   const [map, setMap] = useState<any>(null);
 
   const initialZoom = currentBreakpoint === "xs" ? 10 : 11;
@@ -84,47 +69,6 @@ function GoogleMaps() {
   const onUnmount = useCallback(function callback(map: any) {
     setMap(null);
   }, []);
-
-  /* const allAtmList = fullAtmList
-    .map((atm: rawFetchedNearbyPlacesInfo): IAtmObject => {
-      const atmBrand = getBrandFromRawPlacesInfo(atm);
-
-      // log distances from each ATM
-      const distance = haversine_distance(storedSearchPoint, atm.location);
-      return {
-        brand: atmBrand,
-        name: atm.name,
-        location: atm.location,
-        place_id: atm.place_id,
-        address: atm.vicinity,
-        distance,
-      };
-    })
-    .filter((atm: IAtmObject): boolean => {
-      return !storedBankFilter.includes(atm.brand) && atm.brand !== "";
-    })
-    .sort((atmA, atmB) => atmA.distance! - atmB.distance!); //sort from shortest distance to longest */
-
-  /* const filteredAtmList = fullAtmList
-    .map((atm: rawFetchedNearbyPlacesInfo): IAtmObject => {
-      const atmBrand = getBrandFromRawPlacesInfo(atm);
-
-      // log distances from each ATM
-      const distance = haversine_distance(storedSearchPoint, atm.location);
-      return {
-        brand: atmBrand,
-        name: atm.name,
-        location: atm.location,
-        place_id: atm.place_id,
-        address: atm.vicinity,
-        distance,
-      };
-    })
-    .filter((atm: IAtmObject): boolean => {
-      return !storedBankFilter.includes(atm.brand) && atm.brand !== "";
-    })
-    .sort((atmA, atmB) => atmA.distance! - atmB.distance!) //sort from shortest distance to longest
-    .filter((atm) => atm.distance <= storedRange); //only use ATMs in range */
 
   const handleSelectAtmMarker = (atm: IAtmObject | null) => {
     if (atm === null) {
@@ -141,36 +85,13 @@ function GoogleMaps() {
         (atm) => atm.place_id === storedSelectedAtmId
       );
       if (atmsInFocus.length) mapLookAt(atmsInFocus[0].location);
-      // dispatch(setMapCentrePoint(atmsInFocus[0].location));
     }
   }, [storedSelectedAtmId]);
 
   useEffect(() => {
     mapLookAt(storedSearchPoint);
     mapFitFilteredAtms();
-    //create new static circle
-    // latestSearchRange.current = storedRange;
-    // staticCircle = (
-    //   <CircleF
-    //     options={{
-    //       strokeColor: cupcakeColours.neutral,
-    //       strokeOpacity: 0.8,
-    //       strokeWeight: 2,
-    //       fillColor: cupcakeColours.neutral,
-    //       fillOpacity: 0,
-    //       clickable: false,
-    //       draggable: false,
-    //       editable: false,
-    //       visible: true,
-    //       zIndex: 1,
-    //     }}
-    //     center={storedSearchPoint}
-    //     radius={latestSearchRange.current}
-    //   ></CircleF>
-    // );
   }, [storedSearchPoint]);
-
-  // let staticCircle = null;
 
   const mapLookAt = (point: IGeoCode) => {
     /* console.log(point);
@@ -187,24 +108,7 @@ function GoogleMaps() {
     const bounds = new google.maps.LatLngBounds();
     fullAtmList.forEach((atm) => bounds.extend(atm.location));
     map.fitBounds(bounds);
-    // map.fitBounds(searchCircle.getBounds());
   };
-
-  /*   const searchCircle = new google.maps.Circle({
-    strokeColor: cupcakeColours.primary,
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: cupcakeColours.primary,
-    fillOpacity: 0.1,
-    clickable: false,
-    draggable: false,
-    editable: false,
-    visible: true,
-    zIndex: 1,
-    map,
-    center: storedSearchPoint,
-    radius: storedRange,
-  }); */
 
   return isLoaded ? (
     <GoogleMap
@@ -222,8 +126,6 @@ function GoogleMaps() {
         streetViewControl: false,
       }}
     >
-      {/* Child components, such as markers, info windows, etc. */}
-
       {/* center marking */}
       {searchStarted === true ? (
         <CircleF
@@ -243,9 +145,6 @@ function GoogleMaps() {
           radius={storedRange}
         ></CircleF>
       ) : null}
-
-      {/* center marking static */}
-      {/* {staticCircle} */}
 
       {/* ATMs found */}
       {fullAtmList.map(
