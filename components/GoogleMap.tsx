be497d4423cfe4a99075796816564d5b03d71720
Benjamin Yong <benj_yongzh@hotmail.com>
2023-08-16 23:00:28 +0800

    Move wrapper into GoogleMap. Use Map.tsx as content. Now have to add content (markers and circles).

--- components/GoogleMap.tsx
+++ components/GoogleMap.tsx
@@ -1,194 +1,45 @@
 "use client";
 
-import React, { useState, useCallback, useEffect, useRef } from "react";
-import { GoogleMapsWrapper } from "./GoogleMapsWrapper";
-// import { GoogleMap, useJsApiLoader, CircleF } from "@react-google-maps/api";
+import { ReactElement } from "react";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import Map from "./Map";
 
-//redux
-import { useAppSelector, useAppDispatch } from "@/hooks/reduxHooks";
-import { mapCenterDefault } from "@/features/settings/settingsSlice";
-import { setSelectedAtmPlaceId } from "@/features/atmData/atmDataSlice";
+export default function GoogleMaps() {
+  const apiKey = process.env.NEXT_PUBLIC_GMAPS_API_KEY;
 
-//daisyUI
-import daisyuiColors from "daisyui/src/theming/themes";
-const cupcakeColours = daisyuiColors["[data-theme=cupcake]"];
-const lightColours = daisyuiColors["[data-theme=light]"];
-const nightColours = daisyuiColors["[data-theme=night]"];
+  if (!apiKey) {
+    return (
+      <div className="flex items-center justify-center w-full h-full gap-3">
+        <span>No API key</span>
+      </div>
+    );
+  }
 
-//lib/utils
-import { IGeoCode } from "@/features/googleAPI/geocoder";
-import { IAtmObject } from "@/lib/atmObject";
-import MapMarker from "./MapMarker";
-
-function GoogleMaps() {
-  const ref = useRef<any>(null);
-  //redux
-  const dispatch = useAppDispatch();
-  const storedRange = useAppSelector((state) => state.settings.maxRange);
-  const storedSelectedAtmId = useAppSelector(
-    (state) => state.atmData.selectedAtmPlaceId
-  );
-  const fullAtmList: IAtmObject[] = useAppSelector(
-    (state) => state.atmData.allAtms
-  );
-  const storedSearchPoint: IGeoCode = useAppSelector(
-    (state) => state.settings.searchLocationPoint
-  );
-  const searchStarted: boolean = useAppSelector(
-    (state) => state.atmData.searchStarted
-  );
-  const currentBreakpoint: string = useAppSelector(
-    (state) => state.display.currentBreakpoint
-  );
-
-  //states
-  // const { isLoaded } = useJsApiLoader({
-  //   id: "google-map-script",
-  //   googleMapsApiKey: process.env.NEXT_PUBLIC_GMAPS_API_KEY!, //API key
-  // });
-  const [map, setMap] = useState<google.maps.Map | null>(null);
-
-  const initialZoom = currentBreakpoint === "xs" ? 10 : 11;
-
-  // const onLoad = useCallback(function callback(map: any) {
-  //   // const bounds = new window.google.maps.LatLngBounds(mapCenterDefault);
-  //   // map.fitBounds(bounds);
-  //   map.setZoom(initialZoom);
-  //   setMap(map);
-  // }, []);
-
-  // const onUnmount = useCallback(function callback(map: any) {
-  //   setMap(null);
-  // }, []);
-
-  // useEffect(() => {
-  //   if (storedSelectedAtmId !== null) {
-  //     const atmsInFocus = fullAtmList.filter(
-  //       (atm) => atm.place_id === storedSelectedAtmId
-  //     );
-  //     if (atmsInFocus.length) mapLookAt(atmsInFocus[0].location);
-  //   }
-  // }, [storedSelectedAtmId]);
-
-  // useEffect(() => {
-  //   mapLookAt(storedSearchPoint);
-  //   mapFitFilteredAtms();
-  // }, [storedSearchPoint]);
-
-  // const mapLookAt = (point: IGeoCode) => {
-  //   /* console.log(point);
-  //   console.log(map); */
-  //   if (map) map.panTo(point);
-  // };
-
-  // const mapFitFilteredAtms = () => {
-  //   if (!isLoaded) return;
-  //   if (fullAtmList.length < 1) {
-  //     map.setZoom(initialZoom + 4);
-  //     return;
-  //   }
-  //   const bounds = new google.maps.LatLngBounds();
-  //   fullAtmList.forEach((atm) => bounds.extend(atm.location));
-  //   map.fitBounds(bounds);
-  // };
-
-  // const handleMapClick = (
-  //   event: google.maps.IconMouseEvent | google.maps.MapMouseEvent
-  // ) => {
-  //   event.stop();
-  //   if (!event.placeId) {
-  //     dispatch(setSelectedAtmPlaceId(null));
-  //   } else {
-  //     const selectedPlaceId = fullAtmList.find(
-  //       (atm) => atm.place_id === event.placeId
-  //     );
-  //     if (!selectedPlaceId) {
-  //       dispatch(setSelectedAtmPlaceId(null));
-  //     }
-  //   }
-  // };
-
-  // const marker = new google.maps.marker.AdvancedMarkerElement({
-  //   map,
-  //   position: storedSearchPoint,
-  // });
-
-  // if (!isLoaded) {
-  //   return (
-  //     <div className="flex items-center justify-center w-full h-full gap-3">
-  //       <span>Loading map</span>
-  //       <span className="loading loading-dots loading-md"></span>
-  //     </div>
-  //   );
-  // }
-
-  useEffect(() => {
-    const initGoogleMap = (): google.maps.Map => {
-      return new window.google.maps.Map(ref.current, {
-        center: mapCenterDefault,
-        zoom: initialZoom,
-      });
-    };
-
-    setMap(initGoogleMap());
-    // Display the map
-    // if (ref.current) {
-    //   new window.google.maps.Map(ref.current, {
-    //     center: mapCenterDefault,
-    //     zoom: initialZoom,
-    //   });
-    // }
-  }, [ref]);
+  const render = (status: Status): ReactElement => {
+    if (status === Status.LOADING)
+      return (
+        <div className="flex items-center justify-center w-full h-full gap-3">
+          <span>Loading map</span>
+          <span className="loading loading-dots loading-md"></span>
+        </div>
+      );
+    if (status === Status.FAILURE)
+      return (
+        <div className="flex items-center justify-center w-full h-full gap-3">
+          <span>Loading Failed</span>
+        </div>
+      );
+    return <></>;
+  };
 
   return (
-    <GoogleMapsWrapper>
-      <div ref={ref} style={{ width: "100%", height: "100%" }} />
-    </GoogleMapsWrapper>
+    <Wrapper
+      apiKey={apiKey}
+      version="beta"
+      render={render}
+      libraries={["marker"]}
+    >
+      <Map />
+    </Wrapper>
   );
-
-  // return (
-  //   <GoogleMap
-  //     mapContainerStyle={{
-  //       width: "100%",
-  //       height: "100%",
-  //     }}
-  //     center={mapCenterDefault}
-  //     zoom={initialZoom}
-  //     onLoad={onLoad}
-  //     onUnmount={onUnmount}
-  //     options={{
-  //       gestureHandling: "greedy",
-  //       disableDefaultUI: true,
-  //       streetViewControl: false,
-  //     }}
-  //     onClick={handleMapClick}
-  //   >
-  //     {/* center marking */}
-  //     {searchStarted === true ? (
-  //       <CircleF
-  //         options={{
-  //           strokeColor: "white",
-  //           strokeOpacity: 0.8,
-  //           strokeWeight: 3,
-  //           fillColor: cupcakeColours["base-content"],
-  //           fillOpacity: 0.1,
-  //           clickable: false,
-  //           draggable: false,
-  //           editable: false,
-  //           visible: true,
-  //           zIndex: 1,
-  //         }}
-  //         center={storedSearchPoint}
-  //         radius={storedRange}
-  //       ></CircleF>
-  //     ) : null}
-
-  //     {/* ATMs being looked at */}
-  //     {fullAtmList.map(
-  //       (atm) => isLoaded && <MapMarker atm={atm} key={atm.place_id} />
-  //     )}
-  //   </GoogleMap>
-  // );
 }
-
-export default React.memo(GoogleMaps);
--- components/Map.tsx
+++ components/Map.tsx
@@ -1,122 +1,180 @@
-// "use client";
+"use client";
 
-import { useState } from "react";
-import {
-  GoogleMap,
-  useLoadScript,
-  MarkerF,
-  CircleF,
-  InfoWindow,
-} from "@react-google-maps/api";
+import React, { useState, useCallback, useEffect, useRef } from "react";
+
+//redux
+import { useAppSelector, useAppDispatch } from "@/hooks/reduxHooks";
+import { mapCenterDefault } from "@/features/settings/settingsSlice";
+import { setSelectedAtmPlaceId } from "@/features/atmData/atmDataSlice";
+
+//daisyUI
+import daisyuiColors from "daisyui/src/theming/themes";
+const cupcakeColours = daisyuiColors["[data-theme=cupcake]"];
+const lightColours = daisyuiColors["[data-theme=light]"];
+const nightColours = daisyuiColors["[data-theme=night]"];
+
+//lib/utils
 import { IGeoCode } from "@/features/googleAPI/geocoder";
 import { IAtmObject } from "@/lib/atmObject";
+import MapMarker from "./MapMarker";
 
-import GoogleMapReact from "google-map-react";
+export default function Map() {
+  const mapRef = useRef<HTMLDivElement>(null);
+  const apiKey = process.env.NEXT_PUBLIC_GMAPS_API_KEY;
 
-import { useAppSelector } from "@/hooks/reduxHooks";
-
-type MapProps = {
-  atms: IAtmObject[];
-  zoom: number;
-};
-
-// export default function Map(props: MapProps) {
-//   // infers type from default value
-//   const center: IGeoCode = useAppSelector(
-//     (state) => state.settings.searchLocationPoint
-//   );
-//   const storedRange = useAppSelector((state) => state.settings.maxRange);
-//   const [selectedMarker, setSelectedMarker] = useState<IGeoCode | null>(center);
-//   const { atms } = props;
-//   return (
-//     <div className="mx-20 my-8 md:my-10">
-//       <GoogleMap
-//         zoom={10} //zoom integer setting
-//         center={center}
-//         mapContainerClassName="googlemap-container"
-//       >
-//         {/* center marking */}
-//         <MarkerF
-//           position={center} //marker position
-//           onClick={() => {
-//             setSelectedMarker(center); //marker position
-//           }}
-//         >
-//           {selectedMarker && (
-//             <InfoWindow
-//               onCloseClick={() => {
-//                 setSelectedMarker(null);
-//               }}
-//               position={center} //marker position
-//             >
-//               <p>Directions</p>
-//               <p>This is your input address</p>
-//             </InfoWindow>
-//           )}
-//         </MarkerF>
-
-//         <CircleF
-//           options={{
-//             strokeColor: "hsl(var(--p))",
-//             strokeOpacity: 0.7,
-//             strokeWeight: 2,
-//             fillColor: "hsl(var(--p))",
-//             fillOpacity: 0.2,
-//             clickable: false,
-//             draggable: false,
-//             editable: false,
-//             visible: true,
-//             zIndex: 1,
-//           }}
-//           center={center}
-//           radius={storedRange}
-//         ></CircleF>
-
-//         {/* set atm locations across map */}
-//         {atms.map((atm) => (
-//           <MarkerF
-//             position={atm.location} //marker position
-//             onClick={() => {
-//               setSelectedMarker(atm.location); //marker position
-//             }}
-//           >
-//             {selectedMarker && (
-//               <InfoWindow
-//                 onCloseClick={() => {
-//                   setSelectedMarker(null);
-//                 }}
-//                 position={atm.location} //marker position
-//               >
-//                 <p>{atm.brand}</p>
-//                 <p>{atm.name}</p>
-//                 <p>{atm.address}</p>
-//                 <p>{atm.distance}m away</p>
-//               </InfoWindow>
-//             )}
-//           </MarkerF>
-//         ))}
-//       </GoogleMap>
-//     </div>
-//   );
-// }
-
-export default function Map(props: MapProps) {
-  const center: IGeoCode = useAppSelector(
+  //redux
+  const dispatch = useAppDispatch();
+  const storedRange = useAppSelector((state) => state.settings.maxRange);
+  const storedSelectedAtmId = useAppSelector(
+    (state) => state.atmData.selectedAtmPlaceId
+  );
+  const fullAtmList: IAtmObject[] = useAppSelector(
+    (state) => state.atmData.allAtms
+  );
+  const storedSearchPoint: IGeoCode = useAppSelector(
     (state) => state.settings.searchLocationPoint
   );
-  const storedRange = useAppSelector((state) => state.settings.maxRange);
-  // const [selectedMarker, setSelectedMarker] = useState<IGeoCode | null>(center);
-  const { atms, zoom } = props;
-  return (
-    // Important! Always set the container height explicitly
-    <div className="googlemap-container">
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={center}
-        defaultZoom={zoom}
-      >
-        {/* <AnyReactComponent lat={center.lat} lng={center.lng} text="My Marker" /> */}
-      </GoogleMapReact>
-    </div>
+  const searchStarted: boolean = useAppSelector(
+    (state) => state.atmData.searchStarted
   );
+  const currentBreakpoint: string = useAppSelector(
+    (state) => state.display.currentBreakpoint
+  );
+
+  // const onLoad = useCallback(function callback(map: any) {
+  //   // const bounds = new window.google.maps.LatLngBounds(mapCenterDefault);
+  //   // map.fitBounds(bounds);
+  //   map.setZoom(initialZoom);
+  //   setMap(map);
+  // }, []);
+
+  // const onUnmount = useCallback(function callback(map: any) {
+  //   setMap(null);
+  // }, []);
+
+  // useEffect(() => {
+  //   if (storedSelectedAtmId !== null) {
+  //     const atmsInFocus = fullAtmList.filter(
+  //       (atm) => atm.place_id === storedSelectedAtmId
+  //     );
+  //     if (atmsInFocus.length) mapLookAt(atmsInFocus[0].location);
+  //   }
+  // }, [storedSelectedAtmId]);
+
+  // useEffect(() => {
+  //   mapLookAt(storedSearchPoint);
+  //   mapFitFilteredAtms();
+  // }, [storedSearchPoint]);
+
+  // const mapLookAt = (point: IGeoCode) => {
+  //   /* console.log(point);
+  //   console.log(map); */
+  //   if (map) map.panTo(point);
+  // };
+
+  // const mapFitFilteredAtms = () => {
+  //   if (!isLoaded) return;
+  //   if (fullAtmList.length < 1) {
+  //     map.setZoom(initialZoom + 4);
+  //     return;
+  //   }
+  //   const bounds = new google.maps.LatLngBounds();
+  //   fullAtmList.forEach((atm) => bounds.extend(atm.location));
+  //   map.fitBounds(bounds);
+  // };
+
+  // const handleMapClick = (
+  //   event: google.maps.IconMouseEvent | google.maps.MapMouseEvent
+  // ) => {
+  //   event.stop();
+  //   if (!event.placeId) {
+  //     dispatch(setSelectedAtmPlaceId(null));
+  //   } else {
+  //     const selectedPlaceId = fullAtmList.find(
+  //       (atm) => atm.place_id === event.placeId
+  //     );
+  //     if (!selectedPlaceId) {
+  //       dispatch(setSelectedAtmPlaceId(null));
+  //     }
+  //   }
+  // };
+
+  // const marker = new google.maps.marker.AdvancedMarkerElement({
+  //   map,
+  //   position: storedSearchPoint,
+  // });
+
+  // if (!isLoaded) {
+  //   return (
+  //     <div className="flex items-center justify-center w-full h-full gap-3">
+  //       <span>Loading map</span>
+  //       <span className="loading loading-dots loading-md"></span>
+  //     </div>
+  //   );
+  // }
+
+  //states
+
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+  const initialZoom = currentBreakpoint === "xs" ? 10 : 11;
+
+  useEffect(() => {
+    setMap(
+      new window.google.maps.Map(mapRef.current, {
+        center: mapCenterDefault,
+        zoom: initialZoom,
+        disableDefaultUI: true,
+      })
+    );
+  }, []);
+
+  return (
+    <>
+      <div ref={mapRef} style={{ width: "100%", height: "100%" }} />
+    </>
+  );
+
+  // return (
+  //   <GoogleMap
+  //     mapContainerStyle={{
+  //       width: "100%",
+  //       height: "100%",
+  //     }}
+  //     center={mapCenterDefault}
+  //     zoom={initialZoom}
+  //     onLoad={onLoad}
+  //     onUnmount={onUnmount}
+  //     options={{
+  //       gestureHandling: "greedy",
+  //       disableDefaultUI: true,
+  //       streetViewControl: false,
+  //     }}
+  //     onClick={handleMapClick}
+  //   >
+  //     {/* center marking */}
+  //     {searchStarted === true ? (
+  //       <CircleF
+  //         options={{
+  //           strokeColor: "white",
+  //           strokeOpacity: 0.8,
+  //           strokeWeight: 3,
+  //           fillColor: cupcakeColours["base-content"],
+  //           fillOpacity: 0.1,
+  //           clickable: false,
+  //           draggable: false,
+  //           editable: false,
+  //           visible: true,
+  //           zIndex: 1,
+  //         }}
+  //         center={storedSearchPoint}
+  //         radius={storedRange}
+  //       ></CircleF>
+  //     ) : null}
+
+  //     {/* ATMs being looked at */}
+  //     {fullAtmList.map(
+  //       (atm) => isLoaded && <MapMarker atm={atm} key={atm.place_id} />
+  //     )}
+  //   </GoogleMap>
+  // );
 }
