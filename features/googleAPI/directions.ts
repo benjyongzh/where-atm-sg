7aa30f3373ebad0e53c31d9efd389c7b933b41a8
Benjamin Yong <benj_yongzh@hotmail.com>
2023-09-03 17:10:56 +0800

    Replace origin place id with lat lng

--- features/googleAPI/directions.ts
+++ features/googleAPI/directions.ts
@@ -1,5 +1,7 @@
+import { IGeoCode } from "./geocoder";
+
 export interface IDirections {
-  originPlaceId: string;
+  originLatLng: IGeoCode;
   destinationPlaceId: string;
   mode: string; //walking by default
   duration: number; // in minutes
@@ -7,12 +9,15 @@ export interface IDirections {
 }
 
 export async function getWalkingDirections(
-  originPlaceId: string,
+  originLatLng: IGeoCode,
   destinationPlaceId: string
 ) {
   try {
+    /* const res = await fetch(
+      `https://maps.googleapis.com/maps/api/directions/json?origin=place_id%3A${originPlaceId}&destination=place_id%3A${destinationPlaceId}&mode=walking&avoid=highways&key=${process.env.GMAPS_API_KEY}`
+    ); */
     const res = await fetch(
-      `https://maps.googleapis.com/maps/api/directions/json?destination=place_id%3A${originPlaceId}&origin=place_id%3A${destinationPlaceId}&mode=walking&avoid=highways&key=${process.env.GMAPS_API_KEY}`
+      `https://maps.googleapis.com/maps/api/directions/json?origin=${originLatLng.lat}%2C${originLatLng.lng}&destination=place_id%3A${destinationPlaceId}&mode=walking&avoid=highways&key=${process.env.GMAPS_API_KEY}`
     );
     const data = await res.json();
     return data;
