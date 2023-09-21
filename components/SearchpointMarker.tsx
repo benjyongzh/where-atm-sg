e7344745ba22529a39d57bd57881ae55d9c44f19
Benjamin Yong <benj_yongzh@hotmail.com>
2023-09-21 23:07:21 +0800

    Styling improvement

--- components/SearchpointMarker.tsx
+++ components/SearchpointMarker.tsx
@@ -1,7 +1,7 @@
 const SearchpointMarker = () => {
   return (
     <div
-      className={`cursor-pointer aspect-square w-6 object-center rounded-full border-4 bg-opacity-90 bg-neutral-content scale-100 border-white z-50`}
+      className={`aspect-square w-6 object-center rounded-full border-4 bg-opacity-90 bg-accent scale-100 border-white z-50`}
     ></div>
   );
 };
