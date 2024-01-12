5370fc61da15dd970c46f6116f873de4278643f9
benjyongzh <benj_yongzh@hotmail.com>
2024-01-12 22:06:32 +0800

    Change searchPoint cursor to default

--- components/SearchpointMarker.tsx
+++ components/SearchpointMarker.tsx
@@ -1,7 +1,7 @@
 const SearchpointMarker = () => {
   return (
     <div
-      className={`aspect-square w-6 object-center rounded-full border-4 bg-opacity-90 bg-accent scale-100 border-white z-50`}
+      className={`cursor-default aspect-square w-6 object-center rounded-full border-4 bg-opacity-90 bg-accent scale-100 border-white z-50`}
     ></div>
   );
 };
