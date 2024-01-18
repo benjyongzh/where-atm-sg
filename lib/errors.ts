767acf0cdad0778d2fd544da381e2519691afe31
benjyongzh <benj_yongzh@hotmail.com>
2024-01-18 21:45:49 +0800

    Create function for setting displayed error message

--- lib/errors.ts
+++ lib/errors.ts
@@ -1,7 +1,14 @@
+import { store } from "@/context/store";
+import { setDisplayedErrorMessage } from "@/features/errors/errorsSlice";
+
 export type errorMessageObject = {
   errorMessage: string;
 };
 
 export function isErrorMessageObject(arg: any): arg is errorMessageObject {
   return arg && arg.errorMessage && typeof arg.errorMessage === "string";
+}
+
+export function setDisplayErrorMessage(msg:string | null){
+  store.dispatch(setDisplayedErrorMessage(msg))
 }
