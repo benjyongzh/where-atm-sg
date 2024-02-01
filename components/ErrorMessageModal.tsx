1308793a183f2872dbff461a2918174894d4a5a5
benjyongzh <benj_yongzh@hotmail.com>
2024-02-01 22:10:51 +0800

    Add next steps and bug fixes to do

--- components/ErrorMessageModal.tsx
+++ components/ErrorMessageModal.tsx
@@ -9,26 +9,27 @@ const ErrorMessageModal = () => {
     (state) => state.errors.displayedErrorMessage
   );
 
-  //to add timeout for errormodal
   useEffect(() => {
     console.log("useeffect is run");
     if (errorMessage !== null)
-      setTimeout(() => setDisplayErrorMessage(null, dispatch), 1000);
+      setTimeout(() => setDisplayErrorMessage(null, dispatch), 10000); //TODO timer does not refresh upon new error created
 
     // return () => {
     //   setDisplayErrorMessage(null);
     // };
   }, [errorMessage]);
 
-  if (errorMessage === null) return;
+  if (errorMessage === null) return; //TODO should use framermotion for displaying this component
 
   return (
     <div className="relative z-10 flex flex-col items-stretch justify-start w-full lg:w-[30%]">
       <div
+        // TODO create a onClick event to clear this component
         className={`relative flex flex-col justify-center nav-bg gap-2 item-center`}
       >
         {errorMessage}
-        //TODO it showed '1' when search initiated with zero filtered banks
+        {/* TODO it showed '1' when search initiated with zero filtered banks */}
+        {/* TODO styling of error messages of different severity, if any*/}
       </div>
     </div>
   );
